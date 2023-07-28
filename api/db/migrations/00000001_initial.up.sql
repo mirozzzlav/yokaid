0120cde4f18112254ef01ef53120fa74872c1cb0
miro <miroslav.furinda@gmail.com>
2023-07-28 10:34:27 +0200

    sql
    sqlnew

--- api/db/migrations/00000001_initial.up.sql
+++ api/db/migrations/00000001_initial.up.sql
@@ -22,6 +22,7 @@ CREATE TABLE "posts" (
 	"headline" character varying(256) NOT NULL,
 	"text" text NOT NULL,
 	"created_at" timestamp NOT NULL DEFAULT now(),
+
     CONSTRAINT "posts_pk" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
@@ -63,7 +64,6 @@ ALTER TABLE ONLY "users" ADD CONSTRAINT "users_role_fkey" FOREIGN KEY (role) REF
 ALTER TABLE ONLY "password_change_requests" ADD CONSTRAINT "requests_user_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
 
 /*
-INSERT INTO "users" ("id", "username", "full_name", "email", "hashed_password", "created_at", "role") VALUES
-(3,	'milan',	'milan ko',	'mi@lan.sk',	'$2y$10$qCU7HWIZ6.ovOSLys1PLDOpyMGwCpE7eTqCB5cwtn2WtsO2iHK.1e',	'2023-06-15 16:08:21.721551',	'guest'),
-(1,	'miro',	'mi li nko',	'miro.furo@tuta.io',	'$2a$10$XrcRWW8YabW.hRcSXJSkxugBwCo0AgUthlwAE/Nae2fOW8oJZWBBm',	'2023-06-05 08:02:39.732293',	'professional');
+INSERT INTO "users" ("username", "full_name", "email", "hashed_password", "active", "created_at", "role") VALUES
+('miro',	'miro furo',	'mi@fu.sk',	'$2a$10$yfOG6fThT/U2Mgg2kb/JmuPyDna1epzZjosWfXg6WNPY/4FPxGKnq',	't',	'2023-07-21 15:08:18.440608',	'guest');
 */
---
+++ api/db/migrations/00000002_rental_stuff.down.sql
@@ -0,0 +1,1 @@
+ DROP TABLE user_items, post_images, item_images, images, items, item_categories, rental_posts;
